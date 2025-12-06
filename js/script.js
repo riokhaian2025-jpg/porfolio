756a7d7878a5c013d8d70e721dc0cca299720758
riokhaian2025 <riokhaian2025@gmail.com>
2025-12-06 09:00:39 +0700

    New

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 // Mobile menu toggle
 const menuBtn = document.getElementById('menu-btn');
 const navLinks = document.getElementById('nav-links');
+
 menuBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+    navLinks.classList.toggle('active'); // Hiển thị/ẩn menu
+    menuBtn.classList.toggle('close');   // Thêm dòng này để đổi icon thành X
 });
 
+
+const closeBtn = document.getElementById('close-btn');
+closeBtn.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+});
